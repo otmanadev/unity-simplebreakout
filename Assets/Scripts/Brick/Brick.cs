569cf62cfb4dfe69581445ac31a591d22f7dfce2
Otman A. <otmanazziz.official@gmail.com>
2026-04-05 16:59:20 +0200

    :hammer: Added BricksManager script to handle bricks

--- Assets/Scripts/Brick/Brick.cs
+++ Assets/Scripts/Brick/Brick.cs
@@ -18,11 +18,19 @@ public class Brick : MonoBehaviour
         Assert.IsNotNull(_boxCollider2D);
     }
 
+    private void Start()
+    {
+        Debug.Log($"[Brick / {name}] Send notification to {BricksManager.Instance.name} : Brick created.");
+        BricksManager.Instance.OnBrickCreatedNotification(this);
+    }
+
     /// <summary>
     /// Hit brick, so he could die... Or not...
     /// </summary>
     public void TryHitBrick()
     {
+        Debug.Log($"[Brick / {name}] Send notification to {BricksManager.Instance.name} : Brick destroyed.");
+        BricksManager.Instance.OnBrickDestroyedNotification(this);
         Destroy(gameObject);
     }
     
--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -10,9 +10,19 @@ public class GameManager : MonoBehaviour
         if (Instance != null)
         {
             Debug.LogWarning($"[GameManager / {name}] Instance is not unique : this instance will not be created");
+            Destroy(gameObject);
             return;
         }
         Instance = this;
+    }
+
+    /// <summary>
+    /// Receive notification from BricksManager when no more bricks is up.
+    /// </summary>
+    public void OnNoMoreBricksNotification()
+    {
+        Debug.Log($"[GameManager / {name}] Receive notification from {BricksManager.Instance.name} : No more bricks to destroy.");
+        Debug.Break();
     }
     
 }
