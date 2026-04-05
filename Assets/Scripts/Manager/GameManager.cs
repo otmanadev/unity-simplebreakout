6686f629b499dd28b52477dd9ec9324729caed5d
Otman A. <otmanazziz.official@gmail.com>
2026-04-05 16:32:26 +0200

    :fire: Remove level configuration, so it will be configured by hands

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,16 +1,9 @@
-using System;
-using Domain;
-using NUnit.Framework;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     
     public static GameManager Instance;
-
-    [SerializeField] private TextAsset levelConfigJson;
-    
-    private LevelConfiguration _levelConfig;
 
     private void Awake()
     {
@@ -20,31 +13,6 @@ public class GameManager : MonoBehaviour
             return;
         }
         Instance = this;
-        
-        Assert.IsNotNull(levelConfigJson);
-    }
-
-    private void Start()
-    {
-        //InstanciateLevel();
-        
-        BrickSpawner brickSpawner = BrickSpawner.Instance;
-        if (brickSpawner != null)
-        {
-            //brickSpawner.SpawnBrick();
-        }
-    }
-
-    /// <summary>
-    /// Read level configuration file to create level.
-    /// </summary>
-    private void InstanciateLevel()
-    {
-        _levelConfig = JsonUtility.FromJson<LevelConfiguration>(levelConfigJson.text);
-        Debug.Log($"[GameManager / {name}] Read level config json : {JsonUtility.ToJson(_levelConfig)}");
-        Debug.Log($"[GameManager / {name}] {_levelConfig}");
-        
-        
     }
     
 }
