9f927eb317b19d78de900914ca1b4be3786e2e4d
Otman A. <otmanazziz.official@gmail.com>
2026-04-20 00:44:07 +0200

    :hammer: :art: Updated bricks and brick color generation

--- Assets/Scripts/Brick/Brick.cs
+++ Assets/Scripts/Brick/Brick.cs
@@ -4,25 +4,45 @@ using UnityEngine;
 public class Brick : MonoBehaviour
 {
     
-    private SpriteRenderer _spriteRenderer;
+    private readonly string _materialPropertyColor = "_Color";
+    private readonly string _materialPropertyMainTex = "_MainTex";
     
     private BoxCollider2D _boxCollider2D;
     
+    [Header("Sprites and materials properties")]
+    [SerializeField] private SpriteRenderer outlineRenderer;
+    [SerializeField] private SpriteRenderer gradientRenderer;
+    [SerializeField] private Material gradientMaterial;
+    
+    [Header("Power Up properties")]
     [SerializeField] private GameObject powerUpGO;
     
     private void Awake()
     {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        Assert.IsNotNull(_spriteRenderer);
-        
         _boxCollider2D = GetComponent<BoxCollider2D>();
         Assert.IsNotNull(_boxCollider2D);
+        
+        Assert.IsNotNull(outlineRenderer);
+        Assert.IsNotNull(gradientRenderer);
+        Assert.IsNotNull(gradientMaterial);
+        Assert.IsTrue(gradientMaterial.HasProperty(_materialPropertyColor));
+        PickRandomGradientColor();
     }
 
     private void Start()
     {
         Debug.Log($"[Brick / {name}] Send notification to {BricksManager.Instance.name} : Brick created.");
         BricksManager.Instance.OnBrickCreatedNotification(this);
+    }
+
+    /// <summary>
+    /// Select random color for material gradient color.
+    /// </summary>
+    private void PickRandomGradientColor()
+    {
+        gradientRenderer.material = gradientMaterial;
+        gradientRenderer.material.SetTexture(_materialPropertyMainTex, gradientRenderer.sprite.texture);
+        gradientRenderer.material.SetColor(_materialPropertyColor, BrickColor.PickRandomColor());
     }
     
     /// <summary>
@@ -34,7 +54,7 @@ public class Brick : MonoBehaviour
         PowerUp powerUp = powerUpSO.PowerUpPrefab.gameObject.GetComponent<PowerUp>();
         Debug.Log($"[Brick / {name}] Received power up {powerUp.PowerUpType}.");
         powerUpGO = powerUpSO.PowerUpPrefab;
-        _spriteRenderer.material = powerUpSO.Material;
+        //_spriteRenderer.material = powerUpSO.Material;
     }
 
     /// <summary>
---
+++ Assets/Scripts/Utils/BrickColor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class BrickColor
+{
+    
+    private static List<Color> colors = new List<Color>()
+    {
+        Color.red,
+        Color.green,
+        Color.cyan,
+        Color.blue,
+        Color.magenta,
+        Color.yellow,
+        new Color(1f, .5f, .0f)
+    };
+
+    /// <summary>
+    /// Select random color.
+    /// </summary>
+    /// <returns></returns>
+    public static Color PickRandomColor()
+    {
+        return colors[Random.Range(0, colors.Count)];
+    }
+    
+}
